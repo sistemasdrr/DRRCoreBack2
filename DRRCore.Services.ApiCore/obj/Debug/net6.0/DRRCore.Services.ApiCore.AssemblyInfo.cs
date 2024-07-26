e55831c707f69dc2af330fb0bf65569bb07f2f9d
sistemasdrr <141160460+sistemasdrr@users.noreply.github.com>
2024-07-26 08:45:51 -0500

    merge

--- DRRCore.Services.ApiCore/obj/Debug/net6.0/DRRCore.Services.ApiCore.AssemblyInfo.cs
+++ DRRCore.Services.ApiCore/obj/Debug/net6.0/DRRCore.Services.ApiCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRRCore.Services.ApiCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9443ad5587ef6460079c425ae3c905a2b3dcc5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304bf288c1b97b932508826f3791f5aca6ca16c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRRCore.Services.ApiCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRRCore.Services.ApiCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
