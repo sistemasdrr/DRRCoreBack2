2b15734fa6507ae37789f6c33c7270def4ec3930
sistemasdrr <sistemas@delrisco.com.pe>
2024-10-03 17:32:36 -0500

    vb

--- DRRCore.Services.ApiEmail/obj/Debug/net6.0/DRRCore.Services.ApiEmail.AssemblyInfo.cs
+++ DRRCore.Services.ApiEmail/obj/Debug/net6.0/DRRCore.Services.ApiEmail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRRCore.Services.ApiEmail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4475c60e7269c935b1e423cd7a93d24333ec26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRRCore.Services.ApiEmail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRRCore.Services.ApiEmail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
