b1f69bda6e0a4609c2b5e5be5e9013b24694faf6
sistemasdrr <sistemas@delrisco.com.pe>
2024-11-14 11:25:28 -0500

    dfd

--- DRRCore.Transversal.Mapper/obj/Debug/net6.0/DRRCore.Transversal.Mapper.AssemblyInfo.cs
+++ DRRCore.Transversal.Mapper/obj/Debug/net6.0/DRRCore.Transversal.Mapper.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRRCore.Transversal.Mapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0b1544d1fe264c0f59ef9130e5967da0647ecd")]
->>>>>>> 11/11 1
 [assembly: System.Reflection.AssemblyProductAttribute("DRRCore.Transversal.Mapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRRCore.Transversal.Mapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
