d6f2301a1a5feac2537ae2ac76df5c0fba1a8b77
sistemasdrr <sistemas@delrisco.com.pe>
2025-02-21 09:22:19 -0500

    kkjk

--- DRRCore.Infraestructure.Configuration/SqlContext.cs
+++ DRRCore.Infraestructure.Configuration/SqlContext.cs
@@ -41,8 +41,8 @@ public partial class SqlContext : DbContext
         if (!optionsBuilder.IsConfigured)
         {
             optionsBuilder.UseSqlServer(
-             // "Data Source=200.58.123.184,14331;Initial Catalog=admindrr;User ID=drfero2024x;Password=7KoHVN3ig7mZx;TrustServerCertificate=True"
-             "Data Source=localhost\\DRRSQLSERVER;Initial Catalog=admindrr;User ID=drfero2024x;Password=7KoHVN3ig7mZx;TrustServerCertificate=True"
+            "Data Source=200.58.123.184,14331;Initial Catalog=admindrr;User ID=drfero2024x;Password=7KoHVN3ig7mZx;TrustServerCertificate=True"
+            // "Data Source=localhost\\DRRSQLSERVER;Initial Catalog=admindrr;User ID=drfero2024x;Password=7KoHVN3ig7mZx;TrustServerCertificate=True"
             , sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
                 maxRetryCount: 18,
                 maxRetryDelay: TimeSpan.FromSeconds(60),
